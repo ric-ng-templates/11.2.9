9d97b0de15941baef330347d3f584c2ab77cab34
Richard PYT <rick.info.rsoc@gmail.com>
2021-04-17 13:02:16 +0200

    Rename projet :   model-project  -->  my-project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('model-project app is running!');
+    expect(await page.getTitleText()).toEqual('my-project app is running!');
   });
 
   afterEach(async () => {
